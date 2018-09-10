615827c3190f3bc5fd5f7ea0493da07d5cd44fbf
Stefano Balietti <stbaliet@microsoft.com>
2018-09-10 15:53:33 -0400

    first-commit

---
+++ game/game.stages.js
@@ -0,0 +1,24 @@
+/**
+ * # Game stages definition file
+ * Copyright(c) 2018 Stefano Balietti <<futur.dorko@gmail.com>>
+ * MIT Licensed
+ *
+ * Stages are defined using the stager API
+ *
+ * http://www.nodegame.org
+ * ---
+ */
+
+module.exports = function(stager, settings) {
+
+     stager
+        .next('instructions')
+        .repeat('game', settings.REPEAT)
+        .next('end')
+        .gameover();
+
+    // Modify the stager to skip one stage.
+    // stager.skip('instructions');
+
+    return stager.getState();
+};
